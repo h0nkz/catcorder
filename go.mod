cd7d675550cae7397811fcce58ceb6657eafbc00
bot-anna <102213145+bot-anna@users.noreply.github.com>
2025-05-22 21:36:58 +0200

    Add python test file

--- go.mod
+++
@@ -1,7 +0,0 @@
-module example/hello
-
-go 1.22.0
-
-require github.com/thinkski/go-v4l2 v0.0.0-20220516055852-8bc59635f07d
-
-require golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
---
+++ test.py
@@ -0,0 +1,27 @@
+import cv2
+import socket
+import pickle
+import struct
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(('0.0.0.0', 8888))
+server_socket.listen(5)
+
+print("Server is listening...")
+
+client_socket, client_address = server_socket.accept()
+print(f"Connection from {client_address} accepted")
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    frame_data = pickle.dumps(frame)
+    client_socket.sendall(struct.pack("Q", len(frame_data)))
+    client_socket.sendall(frame_data)
+
+    cv.imshow('Server', frame)
+    if cv2.waitKey(1) == 13:
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
